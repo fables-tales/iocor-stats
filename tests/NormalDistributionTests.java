fbc5d5e8609f450309459752cd198eed0f48b294
Sam Phippen <samphippen@gmail.com>
2009-08-31 15:23:55 +0100

    Added testing
    	new file:   tests/NormalDistributionTests.java
    normal distribution test

---
+++ tests/NormalDistributionTests.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import com.iocor.stats.distributions.NormalDistribution;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class NormalDistributionTests extends TestCase {
+	public void testConstructor() {
+		NormalDistribution d1 = new NormalDistribution();
+		Assert.assertEquals(0.0, d1.Mean());
+		Assert.assertEquals(0.0, d1.Mode());
+		Assert.assertEquals(0.0, d1.Median());
+		Assert.assertEquals(1.0, d1.StandardDeviation());
+		Assert.assertEquals(1.0, d1.Variance());
+		try {
+			NormalDistribution d2 = new NormalDistribution(0.0, -1.0);
+			fail("setting a negative standard devation did not cause an exception");
+		} catch (IllegalArgumentException IAE) {
+
+		}
+		NormalDistribution d3 = new NormalDistribution(0.0, 2.0);
+		Assert.assertEquals(0.0, d3.Mean());
+		Assert.assertEquals(0.0, d3.Mode());
+		Assert.assertEquals(0.0, d3.Median());
+		Assert.assertEquals(2.0, d3.StandardDeviation());
+		Assert.assertEquals(4.0, d3.Variance());
+	}
+}
